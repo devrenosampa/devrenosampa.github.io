a03613ad6b2b09fe31e1f610aab926dc120b04d6
R. Gabriel Sampaio de C. <150556203+devrenosampa@users.noreply.github.com>
2025-01-13 17:07:47 -0300

    gsreposit beta v0.1

--- assets/js/AestheticFluidBg.js
+++
@@ -1,8 +0,0 @@
-<script src="AestheticFluidBg.js"></script>
-
-let colorbg = new Color4Bg.AestheticFluidBg({
-	dom: "box",
-	colors: ["#ffdfb8","#dd7286","#ff5a61","#938da5","#4e65a1","#443d44"],
-	loop: true
-})
-			
