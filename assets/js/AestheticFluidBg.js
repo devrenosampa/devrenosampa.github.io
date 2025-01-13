e42683452b2428eb50fd2c4e4b8b4aac473dab45
R. Gabriel Sampaio de C. <150556203+devrenosampa@users.noreply.github.com>
2025-01-12 21:32:43 -0300

    Upload da rede social Relato

---
+++ assets/js/AestheticFluidBg.js
@@ -0,0 +1,8 @@
+<script src="AestheticFluidBg.js"></script>
+
+let colorbg = new Color4Bg.AestheticFluidBg({
+	dom: "box",
+	colors: ["#ffdfb8","#dd7286","#ff5a61","#938da5","#4e65a1","#443d44"],
+	loop: true
+})
+			
